a9fe4bdef57d7f1c8da4de432ea375d55c0cf1c4
Keisuke Nakano <128650458+honocat@users.noreply.github.com>
2024-10-28 14:14:46 +0900

    fix: 🐛 Process completed with exit code 1
    modifed 'ui/input.tsx', 'ui/textarea.tsx' & 'styles/grid-style.tsx'

--- components/styles/grid-style.tsx
+++ components/styles/grid-style.tsx
@@ -2,7 +2,7 @@ interface GridLayoutProps {
   children: React.ReactNode;
 }
 
-export function GridLayout({ children }: GridLayoutProps) {
+export function GridLayout({ children }: GridLayoutProps): JSX.Element {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{children}</div>
   );
