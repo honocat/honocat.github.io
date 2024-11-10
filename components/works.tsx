c3827cdcf96acaad89542df338c65c25a10a87b0
Keisuke Nakano <128650458+honocat@users.noreply.github.com>
2024-11-11 00:42:42 +0900

    feat: 🎸 data/works.ts
    added data/works.ts for works page

--- components/works.tsx
+++ components/works.tsx
@@ -1,18 +1,16 @@
 import Work from "@/components/work";
+import { works } from "@/data/works";
 
 import { GridLayout } from "@/components/styles/grid-style";
 
 export default function Works(): JSX.Element {
   return (
     <GridLayout>
-      <Work
-        name="QPE - homepage"
-        src="/images/works/yanai-lab.jpg"
-        href="https://yanai-lab.github.io/"
-      >
-        Homepage of the laboratory for Quantitative Political Economy(QPE) I
-        belong to.
-      </Work>
+      {works.map((work, index) => (
+        <Work key={index} name={work.name} src={work.src} href={work.href}>
+          {work.description}
+        </Work>
+      ))}
     </GridLayout>
   );
 }
---
+++ data/works.ts
@@ -0,0 +1,9 @@
+export const works = [
+  {
+    name: "QPE - homepage",
+    src: "/images/works/yanai-lab.jpg",
+    href: "https://yanai-lab.github.io/",
+    description:
+      "Homepage of the laboratory for Quantitative Political Economy(QPE) I belong to.",
+  },
+];
