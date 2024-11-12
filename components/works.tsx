cd46704498883483d2c9a0fa5f5f759006ce4350
Keisuke Nakano <128650458+honocat@users.noreply.github.com>
2024-11-12 14:48:22 +0900

    fix: 🐛 wrong url
    fixed wrong url

--- components/works.tsx
+++ components/works.tsx
@@ -3,14 +3,23 @@ import { works } from "@/data/works";
 
 import { GridLayout } from "@/components/styles/grid-style";
 
+interface WorkType {
+  name: string;
+  src: string;
+  href: string;
+  description: string;
+}
+
 export default function Works(): JSX.Element {
   return (
     <GridLayout>
-      {works.map((work, index) => (
-        <Work key={index} name={work.name} src={work.src} href={work.href}>
-          {work.description}
-        </Work>
-      ))}
+      {works.map(
+        (work: WorkType, index: number): JSX.Element => (
+          <Work key={index} name={work.name} src={work.src} href={work.href}>
+            {work.description}
+          </Work>
+        ),
+      )}
     </GridLayout>
   );
 }
--- data/works.ts
+++ data/works.ts
@@ -2,7 +2,7 @@ export const works = [
   {
     name: "QPE - homepage",
     src: "/images/works/yanai-lab.jpg",
-    href: "https://yanai-lab.github.io/",
+    href: "https://qpe-kut.github.io/",
     description:
       "Homepage of the laboratory for Quantitative Political Economy(QPE) I belong to.",
   },
